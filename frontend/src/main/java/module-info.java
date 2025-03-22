abb0675d8f8c2a9346967e76ac69ae490c982ee7
huutung2004 <huutung7404@gmail.com>
2025-03-22 13:53:12 +0700

    [add] loginAd-Dashboard

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.vinfast.fe {
 
     opens com.vinfast.fe to javafx.fxml;
     exports com.vinfast.fe;
+    opens com.vinfast.controller to javafx.fxml;
+    exports com.vinfast.controller to javafx.fxml;
+
 }
