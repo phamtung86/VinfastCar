69e4c3876cf067ca4bf689207ff3cf027da2f616
phamtung86 <139665713+phamtung86@users.noreply.github.com>
2025-04-08 16:47:08 +0700

    [add] them file .env

--- backend/src/main/java/com/vinfast/Application.java
+++ backend/src/main/java/com/vinfast/Application.java
@@ -1,5 +1,6 @@
 package com.vinfast;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("SECRET", dotenv.get("SECRET"));
 		SpringApplication.run(Application.class, args);
 	}
 
