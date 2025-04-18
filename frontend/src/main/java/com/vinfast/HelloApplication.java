b0b397362806261dae6b7174205133fd2598b402
phamtung86 <139665713+phamtung86@users.noreply.github.com>
2025-04-18 16:37:36 +0700

    [update] update giao dien Manage Car

--- frontend/src/main/java/com/vinfast/HelloApplication.java
+++ frontend/src/main/java/com/vinfast/HelloApplication.java
@@ -7,18 +7,21 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         System.setProperty("javafx.embed.singleThread", "true");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/vinfast/fe/Login.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/vinfast/AdminPage.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(fxmlLoader.load(), 900, 460);
         stage.setTitle("Admin-Login");
         stage.setScene(scene);
         stage.show();
         stage.centerOnScreen();
+
     }
 
     public static void main(String[] args) {
